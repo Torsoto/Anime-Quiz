5d25f0b5397ec84b9adf0f3e64c46946ac81c907
TolgaT <tolga.topal@stud.fh-campuswien.ac.at>
2023-01-31 00:50:33 +0100

    good progress

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var audio = new Audio("/sound/Tenderness.mp3");
-audio.volume = 0.05;
-audio.play();
-audio.loop();
+const header = document.getElementById("#Img");
+const folder = "images/LocationImgs/";
 
 function audioFunction() {
   if (!audio.paused) {
@@ -9,6 +8,18 @@ function audioFunction() {
     document.getElementById("audioImg").src = "/images/audioOFF.png";
   } else {
     document.getElementById("audioImg").src = "/images/audioON.png";
+    audio.volume = 0.05;
     audio.play();
   }
+}
+
+async function getImages() {
+  const response = await fetch("/images/LocationImgs");
+  const text = await response.text();
+  const parser = new DOMParser();
+  const htmlDoc = parser.parseFromString(text, "text/html");
+  const imageArray = Array.from(htmlDoc.querySelectorAll("a"))
+    .filter((a) => a.href.endsWith(".jpg" || ".png"))
+    .map((a) => folder + a.href);
+  header.src = imageArray[Math.floor(Math.random() * imageArray.length)];
 }
