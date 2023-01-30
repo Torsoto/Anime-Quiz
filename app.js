1ccacf83d8a888e7da1be8c1458c5056a42f4ba4
TolgaT <tolga.topal@stud.fh-campuswien.ac.at>
2023-01-30 16:23:30 +0100

    Added Music and changed Layout

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+var audio = new Audio("/sound/Tenderness.mp3");
+audio.volume = 0.05;
+audio.play();
+audio.loop();
 
+function audioFunction() {
+  if (!audio.paused) {
+    audio.pause();
+    document.getElementById("audioImg").src = "/images/audioOFF.png";
+  } else {
+    document.getElementById("audioImg").src = "/images/audioON.png";
+    audio.play();
+  }
+}
